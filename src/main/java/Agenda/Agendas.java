459cfd15434066f621a4d7423560addd10ea72fa
Kevin <Kevin@192.168.0.8>
2021-04-06 17:19:10 -0500

    Agregacion de algunas funciones

--- src/main/java/Agenda/Agendas.java
+++ src/main/java/Agenda/Agendas.java
@@ -2,10 +2,56 @@
 package Agenda;
 
 import Contacto.Contactos;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Agendas {
     
-    private Contactos[] contacto;
+    Scanner dato = new Scanner(System.in);
+    File archivo = new File("Agenda.txt");
+    private Contactos[] contacto; 
+    
+    public Agendas(int tamaño){
+        this.contacto = new Contactos[tamaño];
+    }
+    
+    public void archivoExiste() throws IOException{
+        
+        try {
+            if(archivo.exists()){
+                
+            }
+            else{
+                archivo.createNewFile();
+                
+            }
+        }catch(Exception op){
+            System.out.println(op.getMessage());
+        }
+    }
+    
+    public boolean validarTelefono(String telefono, boolean encontrado){
+        try {
+            BufferedReader leer = new BufferedReader(new FileReader("Agenda.txt"));
+            String x="";
+            encontrado = false;
+            while((x=leer.readLine()) != null){
+                if(x.indexOf(telefono)!= -1){
+                    encontrado = true;
+                    break;
+                }
+            }
+        }catch(Exception op){
+            System.out.println(op.getMessage());
+        }
+        return encontrado;
+    }
+    
+    
+    
     
   
     
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,28 @@
 
+import Agenda.Agendas;
+import Contacto.Contactos;
 import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) {
+           
+        String nombre;
+        String telefono;
+        String correo;
+        String dirrecion;
+        String alias;
         
         
         Integer opcion;
         Scanner dato = new Scanner(System.in);
+        
+        //Agenda agendaTelefonica = new Agenda(100);
+        //agendaTelefonica.leerTexto();
+        //agendaTelefonica.existearchivo();
+        Contactos informacion;
+        
         
         do{
         System.out.println("1) Añadir contacto ");
@@ -28,7 +42,34 @@ public class Main {
         switch(opcion){
  
             case 1: 
+                
+                nombre = dato.nextLine();
+                while(nombre.equals("")){
+                    System.out.println("Ingrese el nombre (Obligatorio) ");
+                    nombre = dato.nextLine(); 
+                }
+                
+                telefono = dato.nextLine();
+                while(telefono.equals("")){
+                    System.out.println("Ingrese el telefono (Obligatorio) ");
+                    telefono = dato.nextLine(); 
+                }
+                
+                System.out.println("Ingrese el correo electronico (Opcional) ");
+                correo = dato.nextLine();
+                
+                System.out.println("Ingrese la dirrecion (Opcional) ");
+                dirrecion = dato.nextLine();
+                
+                System.out.println("Ingrese el alias (Opcional) ");
+                alias = dato.nextLine();
+                
+                informacion = new Contactos(nombre, telefono, correo, dirrecion, alias);
                
+                
+                
+                    
+                
                 break;
                 
             case 2:
